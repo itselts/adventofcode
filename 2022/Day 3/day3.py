1128727956a1fcf1d749f7cc1065d3321b391bfb
Elton Shi <elton1@live.com.au>
2022-12-04 17:54:14 +1100

    part 2

--- 2022/Day 3/day3.py
+++ 2022/Day 3/day3.py
@@ -3,6 +3,7 @@ path = "C:/Users/Elton/Documents/GitHub/adventofcode/2022/Day 3/"
 with open(path+"input.csv") as file:
     data = file.read().splitlines()
 
+# Part 1
 def get_error(input):
     l = len(input)
     first = input[:int(l/2)]
@@ -16,7 +17,7 @@ def get_error(input):
     else:
         return set1.intersection(set2).pop()
 
-print(get_error("vJrwpWtwJgWrhcsFMMfFFhFp"))
+#print(get_error("vJrwpWtwJgWrhcsFMMfFFhFp"))
 
 def get_priority(input):
     if input == None:
@@ -26,10 +27,31 @@ def get_priority(input):
     else:
         return ord(input) - 96
 
-print(get_priority(get_error("vJrwpWtwJgWrhcsFMMfFFhFp")))
+#print(get_priority(get_error("vJrwpWtwJgWrhcsFMMfFFhFp")))
 
 total = 0
 for i in data:
     total += get_priority(get_error(i))
 
+#print(total)
+
+# Part 2
+def get_badge(input):
+    return set(input[0]).intersection(set(input[1]), set(input[2])).pop()
+
+
+# data=['vJrwpWtwJgWrhcsFMMfFFhFp', 'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL', 'PmmdzqPrVvPwwTWBwg', 'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn','ttgJtRGJQctTZtZT','CrZsJsPPZsGzwwsLwLmpwMDw']
+
+group = []
+total = 0
+
+for i in data:
+    if len(group) != 3:
+        group.append(i)
+        continue
+    else:
+        total += get_priority(get_badge(group))
+        group = [i] 
+
+total += get_priority(get_badge(group))
 print(total)
