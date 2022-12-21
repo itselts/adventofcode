ba42376cd61eae462831df8f5935754ef3063aaa
Elton Shi <elton1@live.com.au>
2022-12-21 13:32:44 +1100

    day10

---
+++ 2022/Day 10/day10.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+
+data = [x.split(" ") for x in open("./input.csv").read().splitlines()]
+
+total = 0
+cycle = 0
+X = 1
+
+for line in data:
+    if line[0] == 'noop':
+        cycle += 1
+        if cycle in [20, 60, 100, 140, 180, 220]:
+            total += cycle * X
+        continue
+    else:
+        for _ in range(2):
+            cycle += 1
+            if cycle in [20, 60, 100, 140, 180, 220]:
+                total += cycle * X
+        X += int(line[1])
+
+print(total)
+
+# part 2
+CRT = []
+cycle = 0
+X = 1
+row = []
+
+for line in data:
+    if line[0] == 'noop':
+        cycle += 1
+        pos = cycle-1
+        if pos in range(X-1, X+2):
+            row.append('#')
+        else:
+            row.append('.')
+
+        if cycle == 40:
+            CRT.append(row)
+            cycle = 0
+            row = []
+
+    else:
+        for _ in range(2):
+            cycle += 1
+            pos = cycle-1
+            if pos in range(X-1, X+2):
+                row.append('#')
+            else:
+                row.append('.')
+
+            if cycle == 40:
+                CRT.append(row)
+                cycle = 0
+                row = []
+        X += int(line[1])
+
+print(pd.DataFrame(np.array(CRT)))
