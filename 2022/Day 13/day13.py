5e35e3a6fe4d61074dc40a56ef7f551d5e37fb33
Elton Shi <elton1@live.com.au>
2022-12-24 21:32:32 +1100

    part2

--- 2022/Day 13/day13.py
+++ 2022/Day 13/day13.py
@@ -1,6 +1,9 @@
+import functools
+
 pairs = [[*map(eval, x.split())] for x in open("./input.csv").read().split("\n\n")]
 
 def compare(l, r):
+    # 1 if l < r, 0 if l == r, -1 if l > r
     match l, r:
         case int(), int():
             if l < r:
@@ -34,3 +37,21 @@ for i, pair in enumerate(pairs):
         tot += i+1
 
 print(sum([i+1 for i, pair in enumerate(pairs) if compare(pair[0], pair[1]) == 1]))
+
+
+# part 2
+packets = []
+for a,b in pairs:
+    packets.append(a)
+    packets.append(b)
+
+packets.append([[2]])
+packets.append([[6]])
+
+for i in range(len(packets)):
+    for j in range(len(packets)-1):
+        # print(compare(packets[j], packets[j-1]))
+        if compare(packets[j], packets[j+1]) != 1:
+            packets[j], packets[j+1] = packets[j+1], packets[j]
+
+print(functools.reduce(lambda x, y: x*y, [i+1 for i, x in enumerate(packets) if x == [[2]] or x == [[6]]]))
