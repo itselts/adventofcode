d30696e77c50622c0778df3238a93c6bf659380f
Elton Shi <elton1@live.com.au>
2023-01-02 22:17:16 +1100

    indices*

--- 2022/Day 20/day20.py
+++ 2022/Day 20/day20.py
@@ -3,36 +3,36 @@ encrypted = [int(x) for x in open("./input.csv").read().splitlines()]
 #print(len([i for i, x in enumerate(encrypted) if x==0]))
 #print(len(set(encrypted))) # Values are not unique, except 0!.
 
-indicies = list(range(len(encrypted)))
+indices = list(range(len(encrypted)))
 
 # REMEMBER, the list is circular, so moving a number off one end of the list wraps back around to the other end as if the ends were connected.
 for i, x in enumerate(encrypted):
-    indicies.pop(cur := indicies.index(i))
+    indices.pop(cur := indices.index(i))
     move = (cur+x) % (len(encrypted)-1) # Position (len(encrypted)-1) (AKA end of the list) is the start of the list. move = new index position.
     if move == 0: # Position 0 is the end of the list.
-        indicies.insert(len(encrypted)-1, i)
+        indices.insert(len(encrypted)-1, i)
     else:
-        indicies.insert(move, i)
+        indices.insert(move, i)
 
-decrypted = [encrypted[i] for i in indicies]
+decrypted = [encrypted[i] for i in indices]
 zero_index = decrypted.index(0)
 print(sum([decrypted[(zero_index+x) % len(decrypted)] for x in [1000, 2000, 3000]]))
 
 
 # Part 2
 encrypted2 = [int(x)*811589153 for x in open("./input.csv").read().splitlines()]
-indicies = list(range(len(encrypted2)))
+indices = list(range(len(encrypted2)))
 
 # REMEMBER, the list is circular, so moving a number off one end of the list wraps back around to the other end as if the ends were connected.
 for _ in range(10):
     for i, x in enumerate(encrypted2):
-        indicies.pop(cur := indicies.index(i))
+        indices.pop(cur := indices.index(i))
         move = (cur+x) % (len(encrypted2)-1) # Position (len(encrypted)-1) (AKA end of the list) is the start of the list
         if move == 0: # Position 0 is the end of the list.
-            indicies.insert(len(encrypted2)-1, i)
+            indices.insert(len(encrypted2)-1, i)
         else:
-            indicies.insert(move, i)
+            indices.insert(move, i)
 
-decrypted2 = [encrypted2[i] for i in indicies]
+decrypted2 = [encrypted2[i] for i in indices]
 zero_index2 = decrypted2.index(0)
 print(sum([decrypted2[(zero_index2+x) % len(decrypted2)] for x in [1000, 2000, 3000]]))
